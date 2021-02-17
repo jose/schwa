97bcfbde3d24277c9ca61ec0e5a270f3abb7f21c
jose <jcmcampos@fc.ul.pt>
2021-02-17 09:54:56 +0000

    Attempt to fix "No module named 'pip.req'" (added backward compatibility).

--- setup.py
+++ setup.py
@@ -22,7 +22,10 @@
 
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 import os
 
 dir = os.path.dirname(os.path.abspath(__file__))
